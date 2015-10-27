c867c61a66ff9c967990c607a147b0b201728152
victorgichohi <victorgichohi48@gmail.com>
2015-10-27 14:42:59 +0300

    comments explaining the splash java class

--- app/src/main/java/com/victor/m/eventregistrationsystem/Splash.java
+++ app/src/main/java/com/victor/m/eventregistrationsystem/Splash.java
@@ -9,19 +9,25 @@ import android.os.Handler;
  * Created by victor on 10/27/15.
  */
 public class Splash extends Activity {
-
+//    set the variable for the timeout
     private static int SPLASH_TIME=3000;
 
 
     @Override
+//    onCreate creates the view
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        setContentView sets the content to be viewed which is from splash_screen xml file.
         setContentView(R.layout.splash_screen);
 
+//        this method handles an action once the 3 seconds end
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+//                so it moves to the next screen,login screen when the three seconds are over
                 Intent i = new Intent(Splash.this, LoginActivity.class);
+
+//                variables stating the start of the method and end of it
                 startActivity(i);
 
                 finish();
