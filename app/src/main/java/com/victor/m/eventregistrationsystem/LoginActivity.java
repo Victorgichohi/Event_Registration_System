79052222a249dca789f24c29f3b4aaee1fbfb776
victorgichohi <victorgichohi48@gmail.com>
2015-10-27 15:57:49 +0300

    selected what will happen when one logs in and created an xml for the login

--- app/src/main/java/com/victor/m/eventregistrationsystem/LoginActivity.java
+++ app/src/main/java/com/victor/m/eventregistrationsystem/LoginActivity.java
@@ -1,8 +1,11 @@
 package com.victor.m.eventregistrationsystem;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -11,5 +14,16 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+
+        Button login = (Button) findViewById(R.id.login);
+
+        login.setOnClickListener(new view onClickListener() {
+            @Override
+            public void onClick (View v){
+                intent i = new Intent(LoginActivity.this.EventKeyDetail. class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
